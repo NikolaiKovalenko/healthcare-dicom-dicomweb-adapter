b4bc97cf879c6e0348365b9289b4f0329e0c359f
NiolaiKovalenko <nikolai.k@zazmic.com>
2020-07-22 09:46:56 +0300

    fix delay calculation 2

---
+++ import/src/test/java/com/google/cloud/healthcare/imaging/dicomadapter/DelayCalculatorTest.java
@@ -0,0 +1,34 @@
+package com.google.cloud.healthcare.imaging.dicomadapter;
+
+import com.google.cloud.healthcare.imaging.dicomadapter.backupuploader.DelayCalculator;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DelayCalculatorTest extends Assert {
+    HashMap<Integer, Long> values;
+    @Before
+    public void setup(){
+        values = new HashMap<>(){
+            {
+                put(5, 100L);
+                put(4, 2100L);
+                put(3, 5000L);
+                put(2, 5000L);
+                put(1, 5000L);
+                put(0, 5000L);
+                put(20, 5000L);
+            }};
+    }
+
+    @Test
+    public void delay(){
+        DelayCalculator calc = new DelayCalculator(5,100, 5000);
+        for (Map.Entry<Integer, Long> entry: values.entrySet()) {
+            assertEquals(calc.getExponentialDelayMillis(entry.getKey()), entry.getValue().longValue());
+        }
+    }
+}
