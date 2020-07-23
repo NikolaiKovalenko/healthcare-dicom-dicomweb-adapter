1270ddbe5ba108df7293fe5fc8adcd7a650d9e3e
oleksii opanasenko <oleksii.opanasenko@zazmic.com>
2020-07-23 19:45:03 +0400

    formula code style  changes reverted

--- import/src/main/java/com/google/cloud/healthcare/imaging/dicomadapter/backupuploader/DelayCalculator.java
+++ import/src/main/java/com/google/cloud/healthcare/imaging/dicomadapter/backupuploader/DelayCalculator.java
@@ -29,23 +29,12 @@ public class DelayCalculator {
   public long getExponentialDelayMillis(int attemptsLeft) {
     long delay = 0;
     if (attemptsLeft <= attemptsAmount) {
-      delay =
-          minUploadDelay
-              + (Math.round(
-                          Math.pow(
-                              (double) DELAY_CALCULATION_BASE, attemptsAmount - attemptsLeft + 1))
-                      - DELAY_CALCULATION_BASE)
-                  * MILS_MUL;
+      delay = minUploadDelay + Math.round(Math.pow(DELAY_CALCULATION_BASE, attemptsLeft) - DELAY_CALCULATION_BASE) * MILS_MUL;
     } else {
-      delay =
-          minUploadDelay
-              + (Math.round(Math.pow((double) DELAY_CALCULATION_BASE, attemptsAmount))
-                      - DELAY_CALCULATION_BASE)
-                  * MILS_MUL;
+      delay = minUploadDelay + Math.round(Math.pow(DELAY_CALCULATION_BASE, attemptsAmount) - DELAY_CALCULATION_BASE) * MILS_MUL;
     }
-    long resultDelay =
-        (delay >= (long) maxWaitingTimeBtwUpload) ? (long) maxWaitingTimeBtwUpload : delay;
-    System.out.println("resultDelay=" + resultDelay);
+    long resultDelay = (delay >= (long) maxWaitingTimeBtwUpload) ? (long) maxWaitingTimeBtwUpload : delay;
+
     return resultDelay;
   }
 
