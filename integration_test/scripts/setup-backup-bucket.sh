558c6d4090365e43254587f11d94c6af806960bc
NiolaiKovalenko <nikolai.k@zazmic.com>
2020-08-05 13:02:29 +0300

    fix bugs in bash script

--- integration_test/scripts/setup-backup-bucket.sh
+++ integration_test/scripts/setup-backup-bucket.sh
@@ -3,5 +3,5 @@
 gsutil du -s gs://$3"/ --quiet" >/dev/null
 if [ $? -ne 0 ];
 then
-  gsutil mb -p $1 "-l "$2" -b on gs://"$3" --quiet" || exit $?
+  gsutil mb -p $1 -l $2 -b on "gs://"$3 " --quiet" || exit $?
 fi
